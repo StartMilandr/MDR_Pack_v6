ce9287930214450792fc582cc7a6fcb0e8a71c8a
StartMilandr <Info@StartMilandr.ru>
2019-08-19 10:23:21 +0300

    Timers SVD for ESila, VE8, VK014

--- SVD_Files/Timer/Run_ESila.bat
+++ SVD_Files/Timer/Run_ESila.bat
@@ -1,17 +1,5 @@
 cd C:\CODE\PACK\MDR_Pack\SVD_Files\Exe
 
-del "../Timer/Timer_VE9.txt"
-TextInsert.exe ../Timer/Timer_Common.xml ../Timer/TAGS_VE9.txt 4 #TIMERs
-ren "../Timer/Timer_Common_out.txt" "Timer_VE9.txt
-
-del "../Timer/Timer_VE13.txt"
-TextInsert.exe ../Timer/Timer_Common.xml ../Timer/TAGS_VE13.txt 4 #TIMERs
-ren "../Timer/Timer_Common_out.txt" "Timer_VE13.txt
-
-del "../Timer/Timer_VE4.txt"
-TextInsert.exe ../Timer/Timer_Common.xml ../Timer/TAGS_VE4.txt 4 #TIMERs
-ren "../Timer/Timer_Common_out.txt" "Timer_VE4.txt
-
 del "../Timer/Timer_ES.txt"
 TextInsert.exe ../Timer/Timer_Common.xml ../Timer/TAGS_ES.txt 4 #Timers
 ren "../Timer/Timer_Common_out.txt" "Timer_ES.txt

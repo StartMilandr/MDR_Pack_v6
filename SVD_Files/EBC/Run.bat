772d2af2a0053c2a232e9c75aaca3fe487109ada
StartMilandr <Info@StartMilandr.ru>
2019-12-30 15:34:39 +0300

    ExtBus controller added to SVD for 1986VE8T

---
+++ SVD_Files/EBC/Run.bat
@@ -0,0 +1,6 @@
+cd C:\CODE\PACK\MDR_Pack\SVD_Files\Exe
+
+del "../EBC/EBC_VE8.txt"
+TextInsert.exe ../EBC/EBC_VE8_blank.xml ../EBC/TAGS_VE8.txt 4 #EBC
+ren "../EBC/EBC_VE8_blank_out.txt" "EBC_VE8.txt
+
